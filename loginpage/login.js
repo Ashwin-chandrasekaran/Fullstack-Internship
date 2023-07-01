5036770f8cdc2baa46606bec0beb95ecc6cd156b
Ashwin-chandrasekaran <98824929+Ashwin-chandrasekaran@users.noreply.github.com>
2023-07-02 05:22:09 +0530

    Add files via upload

---
+++ loginpage/login.js
@@ -0,0 +1,9 @@
+const wrapper=document.querySelector(".wrap");
+const loginlink=document.querySelector(".login-link");
+const registerlink=document.querySelector(".register-link");
+registerlink.addEventListener("click",()=>{
+    wrapper.classList.add('active');
+});
+loginlink.addEventListener("click",()=>{
+    wrapper.classList.remove('active');
+});
